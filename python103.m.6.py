35433a7feeb454937b54b22029efddf3bcaf55a7
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-04-03 08:12:42 -0400

    adding exercise 6 from python103-medium

---
+++ python103.m.6.py
@@ -0,0 +1,40 @@
+# Python103, medium, exercise 6
+# Print multiplication tables from 1 - 10
+
+# declaring count var
+count = 1
+
+#declaring array of nums
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(nums[1] * nums[3])
+
+for i in range(10):
+    print('1 x ', (nums[i] * count), ' = ', (1 * (nums[i] * count)))
+
+for i in range(10):
+    print('2 x ', (nums[i] * count), ' = ', (2 * (nums[i] * count)))
+
+for i in range(10):
+    print('3 x ', (nums[i] * count), ' = ', (3 * (nums[i] * count)))
+
+for i in range(10):
+    print('4 x ', (nums[i] * count), ' = ', (4 * (nums[i] * count)))
+
+for i in range(10):
+    print('5 x ', (nums[i] * count), ' = ', (5 * (nums[i] * count)))
+
+for i in range(10):
+    print('6 x ', (nums[i] * count), ' = ', (6 * (nums[i] * count)))
+
+for i in range(10):
+    print('7 x ', (nums[i] * count), ' = ', (7 * (nums[i] * count)))
+
+for i in range(10):
+    print('8 x ', (nums[i] * count), ' = ', (8 * (nums[i] * count)))
+
+for i in range(10):
+    print('9 x ', (nums[i] * count), ' = ', (9 * (nums[i] * count)))
+
+for i in range(10):
+    print('10 x ', (nums[i] * count), ' = ', (10 * (nums[i] * count)))
